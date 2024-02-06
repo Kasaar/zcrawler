92120b6ed86456319ce95f8a9296cb95837572e3
zelle105 <zelle105@umn.edu>
2024-02-06 09:53:43 -0600

    Add main file to execute program, update app accordingly

--- app.py
+++ app.py
@@ -8,106 +8,108 @@ import pymongo
 from urllib.parse import quote_plus
 import sys
 
-# Get the mongo connection info from user
-uri = input("Input your MongoDB uri: ")
+if __name__ == "__main__":
+    def init_mongo():
+        # Get the mongo connection info from user
+        uri = input("Input your MongoDB uri: ")
 
-# Try to connect to mongo
-try:
-    client = pymongo.MongoClient(uri)
-# Return a friendly error if a URI error is thrown
-except pymongo.errors.ConfigurationError:
-  print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
-  sys.exit(1)
+        # Try to connect to mongo
+        try:
+            client = pymongo.MongoClient(uri)
+        # Return a friendly error if a URI error is thrown
+        except pymongo.errors.ConfigurationError:
+            print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
+            sys.exit(1)
 
-db = client["zcrawler_db"]
-queue_col = db["queue"]
-visited_col = db["visited"]
-repository_col = db["repository"]
+        db = client["zcrawler_db"]
+        queue_col = db["queue"]
+        visited_col = db["visited"]
+        repository_col = db["repository"]
 
-start = "https://en.wikipedia.org/wiki/Heart"
+    start = "https://en.wikipedia.org/wiki/Heart"
 
-crawled = []
+    crawled = []
 
-def bfs(url):
-    
-    html_text = requests.get(url).text
-    doc = BeautifulSoup(html_text, 'html.parser')
-    
-    parsed_url = urlparse(url)
-    
-    # Handle each link in the document
-    for link in doc.find_all('a'):
-        l = (link.get('href'))
-
-        if (l is None or len(l) < 2):
-            continue
-            
-        # Handle '/' links
-        elif (len(l) > 1 and l[0] == '/' and l[0:2] != '//'):
-            link_builder = StringIO()
-            link_builder.write(parsed_url.scheme)
-            link_builder.write("://")
-            link_builder.write(parsed_url.hostname)
-            link_builder.write(l)
-            l = link_builder.getvalue()
-            print(l)
-            
-        # Handle '//' links
-        elif (len(l) > 1 and l[0:2] == '//'):
-            link_builder = StringIO()
-            link_builder.write(parsed_url.scheme)
-            link_builder.write(":")
-            link_builder.write(l)
-            l = link_builder.getvalue()
-            print(l)
-            
-        # Handle './' links
-        elif (len(l) > 1 and l[0:2] == './'):
-            link_builder = StringIO()
-            link_builder.write(parsed_url.scheme)
-            link_builder.write("://")
-            link_builder.write(parsed_url.hostname)
-            link_builder.write(dirname(parsed_url.path))
-            link_builder.write(l[1:])
-            l = link_builder.getvalue()
-            print(l)
-            
-        # Handle '#' links
-        elif (l[0] == '#'):
-            link_builder = StringIO()
-            link_builder.write(parsed_url.scheme)
-            link_builder.write("://")
-            link_builder.write(parsed_url.hostname)
-            link_builder.write(parsed_url.path)
-            link_builder.write(l)
-            l = link_builder.getvalue()
-            print(l)
-            
-        # Handle '../' links
-        elif (len(l) > 3 and l[0:3] == '../'):
-            link_builder = StringIO()
-            link_builder.write(parsed_url.scheme)
-            link_builder.write("://")
-            link_builder.write(parsed_url.hostname)
-            link_builder.write('/')
-            link_builder.write(l)
-            l = link_builder.getvalue()
-            print(l)
-            
-        # Handle 'javascript:' links
-        elif (len(l) > 11 and l[0:11] == 'javacript:'):
-            continue
+    def bfs(url):
         
-        # Handle 'mailto:' links
-        elif (len(l) > 7 and l[0:7] == 'mailto:'):
-            continue
+        html_text = requests.get(url).text
+        doc = BeautifulSoup(html_text, 'html.parser')
         
-        # Handle remaining links
-        elif (len(l) > 5 and l[0:5] != 'https' and l[0:4] != 'http'):
-            link_builder = StringIO()
-            link_builder.write(parsed_url.scheme)
-            link_builder.write("://")
-            link_builder.write(parsed_url.hostname)
-            link_builder.write(l)
+        parsed_url = urlparse(url)
         
-bfs(start)
+        # Handle each link in the document
+        for link in doc.find_all('a'):
+            l = (link.get('href'))
+
+            if (l is None or len(l) < 2):
+                continue
+                
+            # Handle '/' links
+            elif (len(l) > 1 and l[0] == '/' and l[0:2] != '//'):
+                link_builder = StringIO()
+                link_builder.write(parsed_url.scheme)
+                link_builder.write("://")
+                link_builder.write(parsed_url.hostname)
+                link_builder.write(l)
+                l = link_builder.getvalue()
+                print(l)
+                
+            # Handle '//' links
+            elif (len(l) > 1 and l[0:2] == '//'):
+                link_builder = StringIO()
+                link_builder.write(parsed_url.scheme)
+                link_builder.write(":")
+                link_builder.write(l)
+                l = link_builder.getvalue()
+                print(l)
+                
+            # Handle './' links
+            elif (len(l) > 1 and l[0:2] == './'):
+                link_builder = StringIO()
+                link_builder.write(parsed_url.scheme)
+                link_builder.write("://")
+                link_builder.write(parsed_url.hostname)
+                link_builder.write(dirname(parsed_url.path))
+                link_builder.write(l[1:])
+                l = link_builder.getvalue()
+                print(l)
+                
+            # Handle '#' links
+            elif (l[0] == '#'):
+                link_builder = StringIO()
+                link_builder.write(parsed_url.scheme)
+                link_builder.write("://")
+                link_builder.write(parsed_url.hostname)
+                link_builder.write(parsed_url.path)
+                link_builder.write(l)
+                l = link_builder.getvalue()
+                print(l)
+                
+            # Handle '../' links
+            elif (len(l) > 3 and l[0:3] == '../'):
+                link_builder = StringIO()
+                link_builder.write(parsed_url.scheme)
+                link_builder.write("://")
+                link_builder.write(parsed_url.hostname)
+                link_builder.write('/')
+                link_builder.write(l)
+                l = link_builder.getvalue()
+                print(l)
+                
+            # Handle 'javascript:' links
+            elif (len(l) > 11 and l[0:11] == 'javacript:'):
+                continue
+            
+            # Handle 'mailto:' links
+            elif (len(l) > 7 and l[0:7] == 'mailto:'):
+                continue
+            
+            # Handle remaining links
+            elif (len(l) > 5 and l[0:5] != 'https' and l[0:4] != 'http'):
+                link_builder = StringIO()
+                link_builder.write(parsed_url.scheme)
+                link_builder.write("://")
+                link_builder.write(parsed_url.hostname)
+                link_builder.write(l)
+            
+    bfs(start)
---
+++ main.py
@@ -0,0 +1,4 @@
+import app
+
+if __name__ == "__main__":
+    input("Specify a directory to crawl. To crawl the entire internet, type '*': ")
