0ccd6c3dd8d05e4b981e699d0e929f2a399a4a42
zelle105 <zelle105@umn.edu>
2024-09-03 15:10:51 -0500

    Fix return type, add http and https link handling

--- link_handler.py
+++ link_handler.py
@@ -9,32 +9,27 @@ class LinkHandler:
             # Handle '/' links
             elif (len(l) > 1 and l[0] == '/' and l[0:2] != '//'):
                 link_builder = [parsed_url.scheme, "://", parsed_url.hostname, l]
-                ''.join(link_builder)
-                return link_builder
+                return ''.join(link_builder)
                 
             # Handle '//' links
             elif (len(l) > 1 and l[0:2] == '//'):
                 link_builder = [parsed_url.scheme, ":", l]
-                ''.join(link_builder)
-                return link_builder
+                return ''.join(link_builder)
                 
             # Handle './' links
             elif (len(l) > 1 and l[0:2] == './'):
                 link_builder = [parsed_url.scheme, "://", parsed_url.hostname, dirname(parsed_url.path), l[1:]]
-                ''.join(link_builder)
-                return link_builder
+                return ''.join(link_builder)
                 
             # Handle '#' links
             elif (l[0] == '#'):
                 link_builder = [parsed_url.scheme, "://", parsed_url.hostname, parsed_url.path, l]
-                ''.join(link_builder)
-                return link_builder
+                return ''.join(link_builder)
                 
             # Handle '../' links
             elif (len(l) > 3 and l[0:3] == '../'):
                 link_builder = [parsed_url.scheme, "://", parsed_url.hostname, '/', l]
-                ''.join(link_builder)
-                return link_builder
+                return ''.join(link_builder)
                 
             # Handle 'javascript:' links
             elif (len(l) > 11 and l[0:11] == 'javacript:'):
@@ -43,12 +38,19 @@ class LinkHandler:
             # Handle 'mailto:' links
             elif (len(l) > 7 and l[0:7] == 'mailto:'):
                 return None
-            
+
+            # Handle 'https://' links
+            elif (len(l) > 8 and l[0:8] == 'https://'):
+                return l
+
+            # Handle 'http://' links
+            elif (len(l) > 7 and l[0:7] == 'http://'):
+                return l
+
             # Handle remaining links
             elif (len(l) > 5 and l[0:5] != 'https' and l[0:4] != 'http'):
                 link_builder = [parsed_url.scheme, "://", parsed_url.hostname, l]
-                ''.join(link_builder)
-                return link_builder
+                return ''.join(link_builder)
 
     def in_domain(self, domain : str, parsed_url : ParseResult) -> bool:
         return 1 if domain == parsed_url.hostname else 0
