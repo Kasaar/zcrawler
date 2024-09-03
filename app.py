780b1dbd067d1b355e1aaaf15cdf60b6f9382996
zelle105 <zelle105@umn.edu>
2024-09-03 15:11:43 -0500

    Major changes to functionality

--- app.py
+++ app.py
@@ -1,142 +1,70 @@
+import link_handler
+import mongo_wrapper
+
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-from io import StringIO
-from os.path import dirname
-
-import pymongo
-from urllib.parse import quote_plus
 import sys
+from tqdm import tqdm
 
 class Crawler:
     def __init__(self):
-        # Get mongo connection info from user
-        uri = input("Input your MongoDB uri: ")
+        self.client_wrapper = mongo_wrapper.MongoWrapper()
+        self.link_fixer = link_handler.LinkHandler()
+        self.domain = None
 
-        # Try to connect to mongo
-        try:
-            client = pymongo.MongoClient(uri)
-        # Return a friendly error if a URI error is thrown
-        except pymongo.errors.ConfigurationError:
-            print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
-            sys.exit(1)
-
-        self.db = client["zcrawler"]
-        self.queue_col = self.db["queue"]
-        self.visited_col = self.db["visited"]
-        self.repository_col = self.db["repository"]
-
-    def get_start(self, input):
-        # Check if any documents exist in the queue
-        if self.queue_col.count_documents({}, limit = 1):
-            output =  self.queue_col.find_one_and_delete({}, sort={'_id': 1}).url
-            print(output)
-            return output
-        elif input == "1":
-            return "https://en.wikipedia.org/wiki/Heart"
+    def start(self, input):
+        if input == "1":
+            start = "https://owenzeller.com"
+            if self.client_wrapper.visited_col.find_one( {"url": start} ) is None and self.client_wrapper.queue_col.find_one( {"url": start} ) is None:
+                self.client_wrapper.queue_col.insert_one( {"url": start} )
+            self.bfs(self.get_next())
         elif input == "2'":
-            return input("Please enter a link to begin at: ")
+            self.domain = input("Please enter the domain to index (e.g. e.wikipedia.org): ")
+            start = input("Please enter a full url within the domain to begin at (e.g. https://en.wikipedia.org/wiki/Main_Page): ")
+            if self.client_wrapper.visited_col.find_one( {"url": start} ) is None and self.client_wrapper.queue_col.find_one( {"url": start} ) is None:
+                self.client_wrapper.queue_col.insert_one( {"url": start} )
+            self.bfs(self.get_next())
         else:
             print("Invalid input to get_start.")
             sys.exit(1)
 
     def get_next(self):
-        # Check if any documents exist in the queue
-        if self.queue_col.count_documents({}, limit = 1):
-            output =  self.queue_col.find_one_and_delete({}, sort={'_id': 1}).url
-            print(output)
+        # Return first existing document in the queue
+        if self.client_wrapper.queue_col.count_documents({}, limit = 1):
+            output = self.client_wrapper.queue_col.find_one_and_delete({}, sort={'_id': 1})["url"]
             return output
+        # Otherwise end program
         else:
             print("Wasn't able to find anything in queue. Stopping.")
+            self.client_wrapper.client.close()
             sys.exit(1)
 
     def bfs(self, url):
-        self.visited_col.insert_one( {"url": url} )
-        html_text = requests.get(url).text
+        self.client_wrapper.visited_col.insert_one( {"url": url} )
+        try:
+            html_text = requests.get(url).text
+        except:
+            print("Failed to open", url)
         doc = BeautifulSoup(html_text, 'html.parser')
         parsed_url = urlparse(url)
         
+        print("Searching links in", url)
+
         # Handle each link in the document
-        for link in doc.find_all('a'):
+        for link in tqdm(doc.find_all('a')):
             l = (link.get('href'))
+            new_l = self.link_fixer.fixup_link(l, parsed_url)
 
-            if self.visited_col.find_one( {"url": url} ) is not None:
-                continue
-
-            elif (l is None or len(l) < 2):
-                continue
-                
-            # Handle '/' links
-            elif (len(l) > 1 and l[0] == '/' and l[0:2] != '//'):
-                link_builder = StringIO()
-                link_builder.write(parsed_url.scheme)
-                link_builder.write("://")
-                link_builder.write(parsed_url.hostname)
-                link_builder.write(l)
-                l = link_builder.getvalue()
-                self.queue_col.insert_one( {"url": url} )
-                print(l)
-                
-            # Handle '//' links
-            elif (len(l) > 1 and l[0:2] == '//'):
-                link_builder = StringIO()
-                link_builder.write(parsed_url.scheme)
-                link_builder.write(":")
-                link_builder.write(l)
-                l = link_builder.getvalue()
-                self.queue_col.insert_one( {"url": url} )
-                print(l)
-                
-            # Handle './' links
-            elif (len(l) > 1 and l[0:2] == './'):
-                link_builder = StringIO()
-                link_builder.write(parsed_url.scheme)
-                link_builder.write("://")
-                link_builder.write(parsed_url.hostname)
-                link_builder.write(dirname(parsed_url.path))
-                link_builder.write(l[1:])
-                l = link_builder.getvalue()
-                self.queue_col.insert_one( {"url": url} )
-                print(l)
-                
-            # Handle '#' links
-            elif (l[0] == '#'):
-                link_builder = StringIO()
-                link_builder.write(parsed_url.scheme)
-                link_builder.write("://")
-                link_builder.write(parsed_url.hostname)
-                link_builder.write(parsed_url.path)
-                link_builder.write(l)
-                l = link_builder.getvalue()
-                self.queue_col.insert_one( {"url": url} )
-                print(l)
-                
-            # Handle '../' links
-            elif (len(l) > 3 and l[0:3] == '../'):
-                link_builder = StringIO()
-                link_builder.write(parsed_url.scheme)
-                link_builder.write("://")
-                link_builder.write(parsed_url.hostname)
-                link_builder.write('/')
-                link_builder.write(l)
-                l = link_builder.getvalue()
-                self.queue_col.insert_one( {"url": url} )
-                print(l)
-                
-            # Handle 'javascript:' links
-            elif (len(l) > 11 and l[0:11] == 'javacript:'):
+            # Do nothing if link has already been visited
+            if self.client_wrapper.visited_col.find_one( {"url": new_l} ) is not None or self.client_wrapper.queue_col.find_one( {"url": new_l} ) is not None:
                 continue
             
-            # Handle 'mailto:' links
-            elif (len(l) > 7 and l[0:7] == 'mailto:'):
-                continue
-            
-            # Handle remaining links
-            elif (len(l) > 5 and l[0:5] != 'https' and l[0:4] != 'http'):
-                link_builder = StringIO()
-                link_builder.write(parsed_url.scheme)
-                link_builder.write("://")
-                link_builder.write(parsed_url.hostname)
-                link_builder.write(l)
-                self.queue_col.insert_one( {"url": url} )
-                print(l)
+            # Add fixed url to bfs queue
+            new_parsed_url = urlparse(new_l)
+            if new_l is not None and (self.domain is None or self.link_fixer.in_domain(self.domain, new_parsed_url)):
+                self.client_wrapper.queue_col.insert_one( {"url": new_l} )
+        
+        # End program if queue is empty, otherwise returns next url and continue bfs
+        next_url = self.get_next()
+        self.bfs(next_url)
