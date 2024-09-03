5231ec08b3ed32254c08b6b0f403c147bd0f5494
zelle105 <zelle105@umn.edu>
2024-09-03 15:10:12 -0500

    Finalize driver code

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ if __name__ == "__main__":
         usr_input = input("Please enter either 1 or 2 to proceed:\n"
             + "=================================================\n"
             + "(1) Crawl the entire web. Will automatically pick up where you left off, or start from the beginning if no existing data is detected.\n"
-            + "(2) Crawl a specific directory. Will request a starting point if no existing data is detected.\n"
+            + "(2) Crawl a specific domain. Will request a starting point if no existing data is detected.\n"
             + "=================================================\n")
 
     crawler = app.Crawler()
-    crawler.bfs(crawler.get_start(usr_input))
-    while 1:
-        crawler.bfs(crawler.get_next())
+    crawler.start(usr_input)
